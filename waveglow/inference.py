d77214ed077617ebe94bb340aed15f303f84e0fe
Jayanta Bhowmick <jbhowmick.et@gmail.com>
2020-04-28 00:02:53 -0400

    Removed submodule, added as tracked files

---
+++ waveglow/inference.py
@@ -0,0 +1,94 @@
+# *****************************************************************************
+#  Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#      * Neither the name of the NVIDIA CORPORATION nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# *****************************************************************************
+import os
+from scipy.io.wavfile import write
+import torch
+from .mel2samp import files_to_list, MAX_WAV_VALUE
+# from .denoiser import Denoiser
+
+
+def load_model(weights_fpath, verbose=True):
+    model = torch.load(weights_fpath)['model']
+    model = model.remove_weightnorm(model)
+    model.cuda().eval()
+
+    return model
+
+
+def is_loaded(model):
+    return model is not None
+
+
+def infer_waveform(mel,
+                   model,
+                   sigma=1.0,
+                   is_fp16=False,
+                   denoiser_strength=0.0):
+    if is_fp16:
+        from apex import amp
+        model, _ = amp.initialize(model, [], opt_level="O3")
+
+    # if denoiser_strength > 0:
+    #     denoiser = Denoiser(_model).cuda()
+
+    mel = torch.autograd.Variable(torch.tensor(mel).cuda())
+    mel = torch.unsqueeze(mel, 0)
+    mel = mel.half() if is_fp16 else mel
+
+    # print(is_fp16)
+    # print(type(mel))
+
+    with torch.no_grad():
+        audio = model.infer(mel, sigma=sigma)
+        # if denoiser_strength > 0:
+        #     audio = denoiser(audio, denoiser_strength)
+        audio = audio * MAX_WAV_VALUE
+
+    audio = audio.squeeze()
+    audio = audio.cpu().numpy()
+    audio = audio.astype('int16')
+
+    return audio
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', "--filelist_path", required=True)
+    parser.add_argument('-w', '--waveglow_path',
+                        help='Path to waveglow decoder checkpoint with model')
+    parser.add_argument("-s", "--sigma", default=1.0, type=float)
+    parser.add_argument("--sampling_rate", default=22050, type=int)
+    parser.add_argument("--is_fp16", action="store_true")
+    parser.add_argument("-d", "--denoiser_strength", default=0.0, type=float,
+                        help='Removes model bias. Start with 0.1 and adjust')
+
+    args = parser.parse_args()
+
+    infer_waveform(args.filelist_path, args.waveglow_path, args.sigma,
+         args.sampling_rate, args.is_fp16, args.denoiser_strength)
